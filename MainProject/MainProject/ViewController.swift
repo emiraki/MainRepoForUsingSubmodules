b66b17081080239e2b44fd4c2be5d09c3afcc380
Emir <>
2022-05-17 14:59:10 +0200

    Initial commit for creating project

---
+++ MainProject/MainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by Emir Hamidovic on 17. 5. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
